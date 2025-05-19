1adc8f57df999c9b668820f3129d034a3f92e8d1
Joran Angevaare <joranangevaare@gmail.com>
2025-05-19 14:20:33 +0200

    py3.10

--- create_environment.sh
+++ create_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "==== install python 3.11 ===="
-conda install python=3.11.6
+conda install python=3.10
 echo "==== install conda-forge libraries ===="
 conda install --file conda_env.txt -vv
 
